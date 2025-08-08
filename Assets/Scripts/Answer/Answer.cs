267881f41ca8ff3738f32889dfcd5dcb45c6890f
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-08-08 13:38:11 +0800

    Добавлено событие выбора ответа

--- Assets/Scripts/Answer/Answer.cs
+++ Assets/Scripts/Answer/Answer.cs
@@ -8,14 +8,19 @@ namespace Answer
         private AnswerData _data;
 
         public event Action DataChanged;
+        public event Action<Answer> Chosen;
 
-        public string LocalizationKey =>
-            _data is not null ? _data.LocalizationKey : string.Empty;
+        public string Id => _data is null ? string.Empty : _data.Id;
+
+        public string LocalizationKey => _data is null ? string.Empty : _data.LocalizationKey;
 
         public void Initialize(AnswerData data)
         {
             _data = data;
             DataChanged?.Invoke();
         }
+
+        public void Choose() =>
+            Chosen?.Invoke(this);
     }
 }
