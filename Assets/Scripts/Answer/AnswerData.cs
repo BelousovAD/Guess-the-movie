45cd5068bcdd1cf0b04c6bc9bdd8b0f661e79c29
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-08-08 13:36:50 +0800

    Добавлено поле Id в данные вопроса

--- Assets/Scripts/Answer/AnswerData.cs
+++ Assets/Scripts/Answer/AnswerData.cs
@@ -5,14 +5,19 @@ namespace Answer
     [CreateAssetMenu(fileName = nameof(AnswerData), menuName = nameof(Answer) + "/" + nameof(AnswerData))]
     public class AnswerData : ScriptableObject
     {
+        [SerializeField] private string _id;
         [SerializeField] private string _localizationKey;
 
-        public string LocalizationKey =>
-            _localizationKey;
+        public string Id => _id;
+        
+        public string LocalizationKey => _localizationKey;
 
 #if UNITY_EDITOR
-        private void OnValidate() =>
+        private void OnValidate()
+        {
+            _id = name;
             _localizationKey = name;
+        }
 #endif
     }
 }
