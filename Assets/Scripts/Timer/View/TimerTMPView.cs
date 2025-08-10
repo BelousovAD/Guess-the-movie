7522d363e699d94cfe3d2640ee498f40017f7288
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-08-10 20:05:47 +0800

    Создан скрипт отображения таймера в тексте

---
+++ Assets/Scripts/Timer/View/TimerTMPView.cs
@@ -0,0 +1,25 @@
+namespace Timer.View
+{
+    using System;
+    using BelousovGameDev.UI.View;
+
+    public class TimerTMPView : AbstractTMPView
+    {
+        private Timer _timer;
+
+        protected void Initialize(Timer timer) =>
+            _timer = timer;
+
+        private void OnEnable()
+        {
+            _timer.Changed += UpdateView;
+            UpdateView();
+        }
+
+        private void OnDisable() =>
+            _timer.Changed -= UpdateView;
+
+        public override void UpdateView() =>
+            TextField.text = TimeSpan.FromSeconds(_timer.GetTimeRemaining()).ToString(Format);
+    }
+}
