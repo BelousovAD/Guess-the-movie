7bbfecd8569e50ecd65d42f655041d2da7fdb4fe
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-08-10 20:10:27 +0800

    Добавлены TimerRunner и HealthRestorer в инсталлер проекта

--- Assets/Scripts/DependencyInjection/ProjectInstaller.cs
+++ Assets/Scripts/DependencyInjection/ProjectInstaller.cs
@@ -5,15 +5,20 @@ namespace DependencyInjection
     using Currency;
     using Question;
     using Reflex.Core;
+    using Timer;
     using Topic;
     using UnityEngine;
 
     public class ProjectInstaller : MonoBehaviour, IInstaller
     {
-        [SerializeField] private int _maxHealth = 5;
+        [SerializeField] private TimerRunner _timerRunner;
+        [SerializeField] private float _secondsBetweenHealthRecovery;
+        [SerializeField] private int _maxHealth;
         
         public void InstallBindings(ContainerBuilder containerBuilder)
         {
+            Health health = new(_maxHealth);
+            
             containerBuilder
                 .AddSingleton(new Topic())
                 .AddSingleton(new Music())
@@ -21,7 +26,9 @@ namespace DependencyInjection
                 .AddSingleton(new AnswerDataList())
                 .AddSingleton(new QuestionDataList())
                 .AddSingleton(new Money())
-                .AddSingleton(new Health(_maxHealth));
+                .AddSingleton(health)
+                .AddSingleton(_timerRunner)
+                .AddSingleton(new HealthRestorer(_timerRunner, health, _secondsBetweenHealthRecovery));
         }
     }
 }
