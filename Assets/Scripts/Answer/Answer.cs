0488ad63dc2fb616a8c9e8ce1954fcd0d0a4b1d0
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-08-11 14:32:03 +0800

    Добавлена фозможность скрывать ответ

--- Assets/Scripts/Answer/Answer.cs
+++ Assets/Scripts/Answer/Answer.cs
@@ -6,13 +6,32 @@ namespace Answer
     public class Answer : MonoBehaviour
     {
         private AnswerData _data;
+        private bool _isVisible = true;
 
         public event Action DataChanged;
         public event Action<Answer> Chosen;
+        public event Action VisibilityChanged;
 
         public string Id => _data is null ? string.Empty : _data.Id;
 
         public string LocalizationKey => _data is null ? string.Empty : _data.LocalizationKey;
+
+        public bool IsVisible
+        {
+            get
+            {
+                return _isVisible;
+            }
+
+            private set
+            {
+                if (value != _isVisible)
+                {
+                    _isVisible = value;
+                    VisibilityChanged?.Invoke();
+                }
+            }
+        }
 
         public void Initialize(AnswerData data)
         {
@@ -22,5 +41,11 @@ namespace Answer
 
         public void Choose() =>
             Chosen?.Invoke(this);
+
+        public void Show() =>
+            IsVisible = true;
+
+        public void Hide() =>
+            IsVisible = false;
     }
 }
